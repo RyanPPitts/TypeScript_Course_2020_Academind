eb535828b145a5534074835792895b5cf16b18c1
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-06-22 10:30:14 -0700

    Debugging
    Typescript shows errors during the dev mode which helsp debug errors before compiling.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('Code goes here');
+function add(n1, n2) {
+    return n1 + n2; // String + Number = 52.8 = concatenate the two variables
+}
+var number1 = '5';
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
 console.log('Code goes here');
+
+function add(n1: number, n2: number) {
+  return n1 + n2; // String + Number = 52.8 = concatenate the two variables
+}
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
