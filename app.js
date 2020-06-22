cbe2f507cf9a97ad9a61385f910734d046076385
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-06-22 10:51:22 -0700

    TypeScript Type : String
    Adding Number, Boolean and String to App.js file to show different typescript types.

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 console.log('Code goes here');
-function add(n1, n2, showResult) {
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
     if (showResult) {
-        console.log(n1 + n2);
+        console.log(phrase + result);
     }
     else {
-        return n1 + n2; // String + Number = 52.8 = concatenate the two variables
+        return result; // String + Number = 52.8 = concatenate the two variables
     }
 }
 // Better to find errors in development instead of runtime - advantage of typescript
 var number1 = 5;
 var number2 = 2.8;
 var printResult = true;
-var result = add(number1, number2, printResult);
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
 console.log('Code goes here');
 
-function add(n1: number, n2: number, showResult: boolean) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
   if (showResult) {
-    console.log(n1 + n2);
+    console.log(phrase + result);
   } else {
-    return n1 + n2; // String + Number = 52.8 = concatenate the two variables
+    return result; // String + Number = 52.8 = concatenate the two variables
   }
 }
 
@@ -13,6 +14,7 @@ function add(n1: number, n2: number, showResult: boolean) {
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2, printResult);
+const result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
