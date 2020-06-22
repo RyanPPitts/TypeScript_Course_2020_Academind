5a97893d4ce36d140f01d9e15922fcd2005177e9
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-06-22 10:40:18 -0700

    TypeScript Core Type : Boolean
    Adding boolean type to the app.js file

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 console.log('Code goes here');
-function add(n1, n2) {
-    return n1 + n2; // String + Number = 52.8 = concatenate the two variables
+function add(n1, n2, showResult) {
+    if (showResult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2; // String + Number = 52.8 = concatenate the two variables
+    }
 }
-var number1 = '5';
+// Better to find errors in development instead of runtime - advantage of typescript
+var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
+var printResult = true;
+var result = add(number1, number2, printResult);
 console.log(result);
--- app.ts
+++ app.ts
@@ -1,10 +1,18 @@
 console.log('Code goes here');
 
-function add(n1: number, n2: number) {
-  return n1 + n2; // String + Number = 52.8 = concatenate the two variables
+function add(n1: number, n2: number, showResult: boolean) {
+  if (showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2; // String + Number = 52.8 = concatenate the two variables
+  }
 }
+
+// Better to find errors in development instead of runtime - advantage of typescript
+
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult);
 console.log(result);
