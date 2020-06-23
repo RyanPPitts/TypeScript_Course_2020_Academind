39c91ebfc0c46ddfbef5a70396b3f2be7ca06f0f
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-06-22 23:36:31 -0700

    Finishing Tutorial
    Great tutorial.  Covers basics of typescript and understand why someone would use typescript.

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var age;
-age = 30;
-var userName = 'Max';
-console.log(userName);
-// all code is compiled down to work on multiple browsers
-//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
-let age: number;
-age = 30;
-const userName = 'Max';
+// let age: number;
+// age = 30;
+// const userName = 'Max';
 
-console.log(userName);
+// console.log(userName);
 
 // all code is compiled down to work on multiple browsers
+let appId = 'abc';
+const button = document.querySelector('button')!;
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
+
+function clickHandler(message: string) {
+  //   let userName = 'Ryan';
+  console.log(`Clicked` + message);
+}
+
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, "You're Welcome"));
+}
